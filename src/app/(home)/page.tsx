e858644d75c61be9b356880b0e7c14ca9f9db101
seob171 <seob9717@gmail.com>
2024-06-17 01:36:17 +0900

    chore: react-error-boundary 설치 / getQueryClient 함수 수정

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,12 +1,13 @@
-import React, { Suspense } from "react";
-import Airport from "@/app/(home)/_components/Airport";
+import React from "react";
+// import Airport from "@/app/(home)/_components/Airport";
+// import { ErrorBoundary } from "react-error-boundary";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col max-w-screen-sm mx-auto bg-slate-400">
-      <Suspense fallback={<div>hello</div>}>
-        <Airport />
-      </Suspense>
+      {/*<ErrorBoundary fallback={<div>ErrorBoundary</div>}>*/}
+      {/*  <Suspense fallback={<div>hello</div>}>/!*<Airport />*!/</Suspense>*/}
+      {/*</ErrorBoundary>*/}
     </main>
   );
 }
--- src/providers/QueryProvider.tsx
+++ src/providers/QueryProvider.tsx
@@ -1,24 +1,38 @@
 "use client";
 
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import {
+  isServer,
+  QueryClient,
+  QueryClientProvider,
+} from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { ReactQueryStreamedHydration } from "@tanstack/react-query-next-experimental";
 
 import { PropsWithChildren, useState } from "react";
 
+function makeQueryClient() {
+  return new QueryClient({
+    defaultOptions: {
+      queries: {
+        staleTime: 60 * 1000,
+      },
+    },
+  });
+}
+
+let browserQueryClient: QueryClient | undefined = undefined;
+
+function getQueryClient() {
+  if (isServer) {
+    return makeQueryClient();
+  } else {
+    if (!browserQueryClient) browserQueryClient = makeQueryClient();
+    return browserQueryClient;
+  }
+}
+
 export default function QueryProvider({ children }: PropsWithChildren) {
-  const [queryClient] = useState(
-    () =>
-      new QueryClient({
-        defaultOptions: {
-          queries: {
-            // With SSR, we usually want to set some default staleTime
-            // above 0 to avoid refetching immediately on the client
-            staleTime: 60 * 1000,
-          },
-        },
-      }),
-  );
+  const queryClient = getQueryClient();
 
   return (
     <QueryClientProvider client={queryClient}>
