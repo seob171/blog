542bb15aade24bd0c4ade6b9a9ede4c5e1de43db
seob171 <seob9717@gmail.com>
2024-08-05 01:16:22 +0900

    style: next-mdx-remote 설정 및 테스트

--- apps/blog/next.config.mjs
+++ apps/blog/next.config.mjs
@@ -3,6 +3,7 @@ import createMDX from '@next/mdx'
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    transpilePackages: ["next-mdx-remote"],
     compiler: {
         removeConsole:
             process.env.NODE_ENV === "production"
