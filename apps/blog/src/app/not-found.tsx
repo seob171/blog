5b420c720458c0bfd86e2f8be768dcbb0cd94f8d
seob171 <seob9717@gmail.com>
2024-08-08 00:18:04 +0900

    style : NotFound 페이지 UI 구현

---
+++ apps/blog/src/app/not-found.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const NotFound = () => {
+  return (
+    <section
+      className={"flex items-center justify-center flex-1 w-full mx-auto"}
+    >
+      <span className={"text-[16rem] font-bold text-muted"}>404</span>
+    </section>
+  );
+};
+
+export default NotFound;
