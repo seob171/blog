a6c49645d2528b83f04e75e310120012eabba79b
seob171 <seob9717@gmail.com>
2024-08-15 00:16:30 +0900

    style: 메타데이터 수정

--- apps/blog/src/app/constants/metadata.ts
+++ apps/blog/src/app/constants/metadata.ts
@@ -3,13 +3,15 @@ import type { Metadata } from "next";
 export const META_DATA = {
   title: "ShimYuseob's blog",
   description:
-    "프론트엔드 3년차 개발자 심유섭입니다. 강아지를 좋아하고 클린코드와 성능개선, 테스트코드에 관심이있습니다. 다양한 기술과 경험을 배워 지식을 공유하고 싶습니다.",
+    "클린코드와 성능개선, 테스트코드에 관심이 많고 강아지를 좋아하는 프론트엔드 개발자의 블로그. 다양한 기술과 경험을 배워 지식을 공유하고 싶습니다.",
   url: "https://shimyuseob.xyz",
 } as const;
 
 export const DEFAULT_META: Metadata = {
   title: META_DATA.title,
   description: META_DATA.description,
+  keywords: ["react", "nextJs", "프론트엔드", "블로그", "개발"],
+  authors: [{ name: "심유섭" }],
   openGraph: {
     title: META_DATA.title,
     description: META_DATA.description,
