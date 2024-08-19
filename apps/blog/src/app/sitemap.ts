9bc95d809b9fabfd3935c24e68d6117d226f88fd
seob171 <seob9717@gmail.com>
2024-08-19 22:53:16 +0900

    feat: 사이트맵 추가

---
+++ apps/blog/src/app/sitemap.ts
@@ -0,0 +1,24 @@
+import type { MetadataRoute } from "next";
+
+import { getBlogPosts } from "@/utils/getBlogPosts";
+
+export default function sitemap(): MetadataRoute.Sitemap {
+  const posts = getBlogPosts();
+
+  const postSiteMap: MetadataRoute.Sitemap = posts.map(({ slug, data }) => ({
+    url: `${process.env.NEXT_PUBLIC_BASE_URL}/post/${decodeURI(slug)}`,
+    lastModified: new Date(data.publishedAt),
+    changeFrequency: "yearly",
+    priority: 0.8,
+  }));
+
+  return [
+    {
+      url: `${process.env.NEXT_PUBLIC_BASE_URL}`,
+      lastModified: new Date(),
+      changeFrequency: "daily",
+      priority: 1,
+    },
+    ...postSiteMap,
+  ];
+}
