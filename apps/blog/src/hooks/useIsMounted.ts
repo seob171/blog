1c4eadd41170470e7c5f6571d067dc4e03abcb6f
seob171 <seob9717@gmail.com>
2024-08-19 23:42:30 +0900

    feat: useIsMounted.ts 구현

---
+++ apps/blog/src/hooks/useIsMounted.ts
@@ -0,0 +1,15 @@
+"use client";
+
+import { useEffect, useState } from "react";
+
+const UseIsMounted = () => {
+  const [isMounted, setIsMounted] = useState(false);
+
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
+
+  return isMounted;
+};
+
+export default UseIsMounted;
