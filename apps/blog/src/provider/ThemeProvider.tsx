01f53f37f55d021282ff0c7590301996e107489f
seob171 <seob9717@gmail.com>
2024-08-19 23:42:58 +0900

    fix: ThemeProvider 서버, 클라이언트 스타일 불일치 이슈 개선

--- apps/blog/src/provider/ThemeProvider.tsx
+++ apps/blog/src/provider/ThemeProvider.tsx
@@ -3,6 +3,15 @@
 import { ThemeProvider as NextThemesProvider } from "next-themes";
 import { type ThemeProviderProps } from "next-themes/dist/types";
 
+import UseIsMounted from "@/hooks/useIsMounted";
+
 export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
-  return <NextThemesProvider {...props}>{children}</NextThemesProvider>;
+  const isMounted = UseIsMounted();
+  const isProduction = process.env.NODE_ENV === "production";
+
+  if (isProduction || isMounted) {
+    return <NextThemesProvider {...props}>{children}</NextThemesProvider>;
+  }
+
+  return children;
 }
