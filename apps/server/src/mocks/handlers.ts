873d25aa15183dc937ef8410c8e2a812a570371b
seob171 <seob9717@gmail.com>
2024-08-22 23:31:13 +0900

    test: vite 환경에서 msw 사용해보기

--- apps/server/src/mocks/handlers.ts
+++ apps/server/src/mocks/handlers.ts
@@ -1,25 +1,3 @@
-// import { http, HttpResponse } from "msw";
-//
-// type Params = Record<string, string>;
-// type RequestBody = Record<string, string>;
-// type ResponseBody = {
-//   id: string;
-//   firstName: string;
-//   lastName: string;
-// };
-// type Path = "/user";
-//
-// export const handler = [
-//   http.get<Params, RequestBody, ResponseBody, Path>("/user", () => {
-//     return HttpResponse.json({
-//       id: "c7b3d8e0-5e0b-4b0f-8b3a-3b9f4b3d3b3d",
-//       firstName: "John",
-//       lastName: "Maverick",
-//     });
-//   }),
-// ];
-
-// src/mocks/handlers.js
 import { http, HttpResponse } from "msw";
 
 export const handlers = [
