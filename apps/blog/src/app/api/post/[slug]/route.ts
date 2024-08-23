bf257783a5e50c457d66224607f226f0d137415d
seob171 <seob9717@gmail.com>
2024-08-24 00:00:25 +0900

    refactor: post routes > 에러핸들링

--- apps/blog/src/app/api/post/[slug]/route.ts
+++ apps/blog/src/app/api/post/[slug]/route.ts
@@ -8,34 +8,58 @@ export const GET = async (
   request: NextRequest,
   { params: { slug } }: { params: { slug: string } },
 ) => {
-  const post = await prisma.posts.findUnique({
-    where: { slug },
-  });
+  try {
+    const post = await prisma.posts.findUnique({
+      where: { slug },
+    });
 
-  return NextResponse.json(post, { status: 200 });
+    if (!post) {
+      return NextResponse.json(null, { status: 404 });
+    }
+
+    return NextResponse.json(post, { status: 200 });
+  } catch (error) {
+    return NextResponse.json({ error }, { status: 500 });
+  }
 };
 
 export const POST = async (
   request: NextRequest,
   { params: { slug } }: { params: { slug: string } },
 ) => {
-  const post = await prisma.posts.create({
-    data: { slug, views: 0 },
-  });
+  try {
+    const post = await prisma.posts.create({
+      data: { slug, views: 0 },
+    });
 
-  return NextResponse.json(post, { status: 200 });
+    if (!post) {
+      return NextResponse.json(null, { status: 404 });
+    }
+
+    return NextResponse.json(post, { status: 200 });
+  } catch (error) {
+    return NextResponse.json({ error }, { status: 500 });
+  }
 };
 
 export const PATCH = async (
   request: NextRequest,
   { params: { slug } }: { params: { slug: string } },
 ) => {
-  const { views } = (await request.json()) as PrismaModels["posts"];
+  try {
+    const { views } = (await request.json()) as PrismaModels["posts"];
 
-  const post = await prisma.posts.update({
-    where: { slug },
-    data: { views },
-  });
+    const post = await prisma.posts.update({
+      where: { slug },
+      data: { views },
+    });
 
-  return NextResponse.json(post, { status: 200 });
+    if (!post) {
+      return NextResponse.json(null, { status: 404 });
+    }
+
+    return NextResponse.json(post, { status: 200 });
+  } catch (error) {
+    return NextResponse.json({ error }, { status: 500 });
+  }
 };
