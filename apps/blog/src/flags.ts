16df870e2010dc937187a892a0f23c729d24329d
seob171 <seob9717@gmail.com>
2024-10-27 16:46:28 +0900

    test : feature flag name 변경

--- apps/blog/src/flags.ts
+++ apps/blog/src/flags.ts
@@ -2,13 +2,13 @@ import { get, has } from '@vercel/edge-config';
 import { unstable_flag as flag } from '@vercel/flags/next';
 
 export const showSummerSale = flag({
-  key: 'summer-sale',
+  key: 'newFeature',
   async decide() {
-    const exists = await has('summer-sale');
+    const exists = await has('newFeature');
 
     if (!exists) return false;
 
-    const value = await get('summer-sale'); // could use this.key instead
+    const value = await get('newFeature'); // could use this.key instead
     return value ?? false;
   },
 });
