ab838ee8bc8b471ab4f7a2a9bc21cb9a5b253190
seob171 <seob9717@gmail.com>
2024-10-31 01:41:25 +0900

    test : 테스트 코드

--- apps/blog/src/app/write/page.tsx
+++ apps/blog/src/app/write/page.tsx
@@ -9,9 +9,8 @@ const Page = async () => {
     return (
       <div className={'flex flex-col gap-2'}>
         <span>Write Page 준비중...</span>
-        <span>테스트 코드 작성1</span>
-        <span>테스트 코드 작성2</span>
         <span>테스트 코드 작성3</span>
+        <span>테스트 코드 작성1</span>
         <span>테스트 코드 작성4</span>
       </div>
     );
