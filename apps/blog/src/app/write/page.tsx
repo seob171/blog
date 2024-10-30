467d79e20b9247ccd60f35d635cc5923c1a6cf58
seob171 <seob9717@gmail.com>
2024-10-31 01:31:24 +0900

    test : 슬랙 연동 테스트 코드

--- apps/blog/src/app/write/page.tsx
+++ apps/blog/src/app/write/page.tsx
@@ -14,7 +14,6 @@ const Page = async () => {
         <span>테스트 코드 작성</span>
         <span>테스트 코드 작성</span>
         <span>테스트 코드 작성</span>
-        <span>테스트 코드 작성</span>
       </div>
     );
   }
